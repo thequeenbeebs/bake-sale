52e0214a9e9ab589dd5408cc007c99497f2327f4
Blaire Baker <bakerblaire@gmail.com>
2021-05-24 14:51:12 -0600

    fetch items and set state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/items')
+      .then(resp => resp.json())
+      .then(data => setItems(data))
+  })
   return (
     <div className="App">
       Bake Sale
+      <ul>
+        {items.map(item => <img src={item.img}></img>)}
+      </ul>
     </div>
   );
 }
